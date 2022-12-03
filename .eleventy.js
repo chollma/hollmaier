55752d6ac0ea360627ac0d4eedf1063b8406ed0b
chollma <53924848+chollma@users.noreply.github.com>
2022-12-03 12:22:53 -0600

    Trying to deploy to github pages again

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     return {
+        pathPrefix: "/hollmaier/",
         dir: {
             input: "src",
             output: "public",
