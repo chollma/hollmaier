e695b231644c4506e6ad829be220315e98de684d
chollma <53924848+chollma@users.noreply.github.com>
2022-12-03 12:36:38 -0600

    Reset

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     return {
-        pathPrefix: "/hollmaier/",
         dir: {
             input: "src",
             output: "public",
