d40be615960c3b66d99ba9c6adc002674ac8fdf1
chollma <53924848+chollma@users.noreply.github.com>
2022-12-03 12:55:15 -0600

    Add redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/_redirects");
     return {
         dir: {
             input: "src",
