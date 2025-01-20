7408414184d5969d4b662e8e66b4bef7a08ca13b
chollma <53924848+chollma@users.noreply.github.com>
2025-01-20 14:33:56 -0600

    Adding persistent bio/about to footer and optimizing styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/_redirects");
+    eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/docs");
     return {
         dir: {
             input: "src",
